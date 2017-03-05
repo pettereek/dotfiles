9cb4dda5dbc7a41ba493a27f2f8df1c32ee1ea68
Petter Eek <petter@pettereek.se>
2017-03-05 20:56:17 +0100

    [hammerspoon] cleanup, bail if highlight not available

--- hammerspoon/highlight/highlight.lua
+++ hammerspoon/highlight/highlight.lua
@@ -2,40 +2,62 @@
 local log = hs.logger.new('highlight','debug')
 
 local executeAppleScript = function(file)
-  local path = hs.configdir .. '/highlight/' .. file
-  local code, _status = hs.execute('cat ' .. path)
+  local code, _status = hs.execute('cat ' .. hs.configdir .. '/highlight/' .. file)
   local _success, result, _code = hs.osascript.applescript(code)
+
+  if result == 'false' then
+    return
+  end
 
   return result
 end
 
-hs.hotkey.bind(hyper, "H", function()
-  local pb = hs.pasteboard.readString()
+local assertExecutable = function(name)
+  local result, status = hs.execute('command -v ' .. name, true)
+  local executable = hs.fnutils.split(result, '%s', 2)[1]
 
-  if pb == nil then
-    log.w('Empty pasteboard')
-    return
+  if status == nil or executable == '' then
+    return '', false
   end
 
-  -- Write contents to tmp file for easier use of highlighter
+  return executable, true
+end
+
+local abort = function(reason)
+  log.w(reason .. ', aborting...')
+end
+
+hs.hotkey.bind(hyper, "H", function()
+  local executable, ok = assertExecutable('highlight')
+  if ok == false then
+    return abort('Executable "highlight" not found')
+  end
+
+  local pb = hs.pasteboard.readString()
+  if pb == nil then
+    return abort('Empty pasteboard')
+  end
+
+  -- Write contents to tmp file, much easier for highlight to handle
   file = assert(io.open(os.getenv("HOME")..'/.highlight.tmp', 'w'))
   file:write(pb)
 
   local syntax = executeAppleScript('syntax.AppleScript')
-  if syntax == false then
-    log.w('Aborted...')
+  if syntax == nil then
+    return abort('No syntax')
   end
 
   local style = executeAppleScript('style.AppleScript')
-  if style == false then
-    log.w('Aborted...')
+  if style == nil then
+    return abort('No style')
   end
 
   log.i('syntax "' .. syntax .. '", style "', style .. '"')
 
-  local cmd = '/usr/local/bin/highlight -i ~/.highlight.tmp -O rtf --line-numbers --replace-tabs=2 --font-size=32 --font=Monaco --no-trailing-nl --syntax=' .. syntax .. ' --style=' .. style
-  local highlighted, _status, _type, _rc = hs.execute(cmd, false)
+  local cmd = executable .. ' -i ~/.highlight.tmp -O rtf --line-numbers --replace-tabs=2 --font-size=32 --font=Monaco --no-trailing-nl --syntax=' .. syntax .. ' --style=' .. style
+  local highlighted, _status, _type, _rc = hs.execute(cmd)
 
   local styledText = hs.styledtext.getStyledTextFromData(highlighted, 'rtf')
   hs.pasteboard.writeObjects(styledText)
+  hs.notify.show('Highlight copied! 👻', '', 'Highlighted text has been added to the clipboard.')
 end)
