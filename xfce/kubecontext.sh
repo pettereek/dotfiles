5e57faf835408a41bb09c79d22121b64c5420111
Petter Machado <petter@machado.se>
2019-07-25 10:09:47 +0200

    Add tool, txtclick to kubecontext.sh.

--- xfce/kubecontext.sh
+++ xfce/kubecontext.sh
@@ -1,11 +1,43 @@
 #!/usr/bin/env bash
-kubectl_current_context=$(cat ~/.kube/config | grep -Po 'current-context: \K.*')
-kubectl_cluster=$(echo $kubectl_current_context | cut -d '_' -f 4)
-kubectl_project=$(echo $kubectl_current_context | cut -d '_' -f 2 | grep -Po 'syb-\K.*')
 
-icon=""
-if [[ "$kubectl_project" == *production* ]]; then
-  icon="🚨 "
+# A Genmon (xfce4-genmon-plugin) plugin for Kuberenetes.
+#
+# Displays the current context and opens the Kuberenetes Engine/Workloards
+# page on click.
+#
+# For more information see:
+# https://goodies.xfce.org/projects/panel-plugins/xfce4-genmon-plugin
+
+# Colorize the text if we are in a production cluster.
+PRODUCTION_COLOR="#a9dd9d"
+
+# Font for the panel text label.
+PANEL_FONT="Monospace Bold 9"
+
+api_version=$(cat $HOME/.kube/config | grep -Po 'apiVersion: \K.*')
+current_context=$(cat $HOME/.kube/config | grep -Po 'current-context: \K.*')
+cluster=$(echo $current_context | cut -d '_' -f 4)
+region=$(echo $current_context | cut -d '_' -f 3)
+project=$(echo $current_context | cut -d '_' -f 2)
+
+fgcolor=""
+if [[ "$project" == *production* ]]; then
+  fgcolor="fgcolor='$PRODUCTION_COLOR'"
 fi
 
-echo "<txt>$icon$kubectl_project/$kubectl_cluster</txt>"
+k8s_url="https://console.cloud.google.com/kubernetes/workload?project=$project"
+
+echo "<txt><span font='$PANEL_FONT' $fgcolor>$project/$cluster</span></txt>"
+echo "<txtclick>xdg-open $k8s_url</txtclick>"
+echo -ne "<tool>"
+echo -ne "<span font='Monospace'>"
+echo -ne "Project     <span font_weight='bold'>$project</span>\n"
+echo -ne "Cluster     <span font_weight='bold'>$cluster</span>\n"
+echo -ne "Region      <span font_weight='bold'>$region</span>\n"
+echo -ne "API Version <span font_weight='bold'>$api_version</span>\n"
+echo -ne "\n"
+echo -ne "Context\n$current_context\n"
+echo -ne "</span>"
+echo -ne "\n"
+echo -ne "<span font_style='italic'>Click to open Kuberenetes Engine in $project.</span>"
+echo -ne "</tool>"
